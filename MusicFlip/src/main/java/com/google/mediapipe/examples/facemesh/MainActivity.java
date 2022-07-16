12d90bf4a8dc84178a15d9778c5fdb2122ef35df
Hayden2018 <39459225+Hayden2018@users.noreply.github.com>
2022-07-16 15:14:40 +0800

    fix error

--- MusicFlip/src/main/java/com/google/mediapipe/examples/facemesh/MainActivity.java
+++ MusicFlip/src/main/java/com/google/mediapipe/examples/facemesh/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
   private boolean shakeEnable = false;
 
     private static final int DEFAULT_BLINK_SENSITIVITY = 80;
-    private int blinkSensitivity = DEFAULT_BLINK_SENSITIVITY;
+    private float blinkSensitivity = DEFAULT_BLINK_SENSITIVITY;
 
     protected long coolDown = System.currentTimeMillis();
 
@@ -60,23 +60,44 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
   public ViewFragment viewFragment;
   public CollectionFragment collectionFragment = CollectionFragment.newInstance();
-  public SettingFragment settingFragment = SettingFragment.newInstance("", "");
+  public SettingsFragment settingFragment = SettingsFragment.newInstance();
 
   private BottomNavigationView bottomNavigationView;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    setupLiveDemoUiComponents();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupLiveDemoUiComponents();
 
-    bottomNavigationView = findViewById(R.id.bottom_navigation);
+        loadSettings();
 
-    bottomNavigationView.setSelectedItemId(R.id.view_button);
-    bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-    viewFragment = (ViewFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
-  }
+        bottomNavigationView.setSelectedItemId(R.id.view_button);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        viewFragment = (ViewFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (detectionEnable) {
+            cameraInput = new CameraInput(this);
+            cameraInput.setNewFrameListener(textureFrame -> facemesh.send(textureFrame));
+            startCamera();
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (detectionEnable) {
+            cameraInput.close();
+        }
+    }
 
   @Override
   public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -112,78 +133,11 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         transaction.replace(R.id.fragment, settingFragment).commit();
         return true;
     }
-
-    Enum<Current> current = Current.VIEW;
-
-    public ViewFragment viewFragment;
-    public CollectionFragment collectionFragment = CollectionFragment.newInstance();
-    public SettingsFragment settingFragment = SettingsFragment.newInstance();
-
-    private BottomNavigationView bottomNavigationView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setupLiveDemoUiComponents();
-
-        loadSettings();
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setSelectedItemId(R.id.view_button);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-
-        viewFragment = (ViewFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+    return false;
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        switch (item.getItemId()) {
-            case R.id.collections_button:
-                current = Current.COLLECTION;
-                setTitle(getString(R.string.collections_title));
-                transaction.replace(R.id.fragment, collectionFragment).commit();
-                return true;
-
-            case R.id.view_button:
-                current = Current.VIEW;
-                setTitle(getString(R.string.view_title));
-                transaction.replace(R.id.fragment, viewFragment).commit();
-                return true;
-
-            case R.id.settings_button:
-                current = Current.SETTING;
-                setTitle(getString(R.string.setting_title));
-                transaction.replace(R.id.fragment, settingFragment).addToBackStack(null).commit();
-                return true;
-        }
-        return false;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (detectionEnable) {
-            cameraInput = new CameraInput(this);
-            cameraInput.setNewFrameListener(textureFrame -> facemesh.send(textureFrame));
-            startCamera();
-
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (detectionEnable) {
-            cameraInput.close();
-        }
-    }
-
-    protected void triggerRight(Enum<Source> source) {
+    protected void triggerRight() {
         coolDown = System.currentTimeMillis();
 
         if (current == Current.VIEW) {
@@ -191,7 +145,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
     }
 
-    protected void triggerLeft(Enum<Source> source) {
+    protected void triggerLeft() {
         coolDown = System.currentTimeMillis();
 
         if (current == Current.VIEW) {
@@ -236,18 +190,20 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                     if (System.currentTimeMillis() - coolDown < 1000) {
                         return;
                     }
-                    Enum<Blink> blink = detectBlink(faceMeshResult);
-                    Enum<Shake> shake = detectShake(faceMeshResult);
+                    List<NormalizedLandmark> landmarks = faceMeshResult.multiFaceLandmarks().get(0).getLandmarkList();
+                    detector.setTransformMatrix(landmarks);
+                    Enum<Blink> blink = detector.detectBlink(landmarks);
+                    Enum<Shake> shake = detector.detectShake(landmarks);
                     if (shake == Shake.LEFT) {
-                        if (shakeEnable) triggerLeft(Source.SHAKE);
+                        if (shakeEnable) triggerLeft();
                         return;
                     }
                     if (shake == Shake.RIGHT) {
-                        if (shakeEnable) triggerRight(Source.SHAKE);
+                        if (shakeEnable) triggerRight();
                         return;
                     }
-                    if (blink == Blink.LEFT) triggerLeft(Source.BLINK);
-                    if (blink == Blink.RIGHT) triggerRight(Source.BLINK);
+                    if (blink == Blink.LEFT) triggerLeft();
+                    if (blink == Blink.RIGHT) triggerRight();
                 });
 
         if (detectionEnable) {
@@ -265,45 +221,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         );
     }
 
-    final List<Integer> rightEyeUpper = Arrays.asList(398, 384, 385, 386, 387, 388, 466);
-    final List<Integer> rightEyeLower = Arrays.asList(382, 381, 380, 374, 373, 390, 249);
-    final List<Integer> leftEyeUpper = Arrays.asList(246, 161, 160, 159, 158, 157, 173);
-    final List<Integer> leftEyeLower = Arrays.asList(7, 163, 144, 145, 153, 154, 155);
-
-    private Enum<Blink> detectBlink(FaceMeshResult result) {
-
-        if (!blinkEnable) return Blink.NONE;
-
-        List<NormalizedLandmark> landmarks = result.multiFaceLandmarks().get(0).getLandmarkList();
-        float right = 0;
-        float left = 0;
-        float tilt = landmarks.get(0).getX() - landmarks.get(164).getX();
-
-        for (int i = 0; i < 7; ++i) {
-            right += abs(landmarks.get(rightEyeUpper.get(i)).getY() - landmarks.get(rightEyeLower.get(i)).getY());
-            left += abs(landmarks.get(leftEyeUpper.get(i)).getY() - landmarks.get(leftEyeLower.get(i)).getY());
-        }
-
-        if (right > (left + 0.03) && tilt < -0.008) return Blink.LEFT;
-        if (left > (right + 0.03) && tilt > 0.008) return Blink.RIGHT;
-        return Blink.NONE;
-    }
-
-    private Enum<Shake> detectShake(FaceMeshResult result) {
-
-        List<NormalizedLandmark> landmarks = result.multiFaceLandmarks().get(0).getLandmarkList();
-
-        float dz = landmarks.get(454).getZ() - landmarks.get(234).getZ();
-        if (dz > 0.12) return Shake.RIGHT;
-        if (dz < -0.12) return Shake.LEFT;
-        return Shake.NONE;
-    }
-
     // load settings by reading the shared preferences
     protected void loadSettings() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         shakeEnable = sharedPreferences.getBoolean("detect_head_shake_preference", false);
-        blinkSensitivity = sharedPreferences.getInt("blink_sensitivity_preference", DEFAULT_BLINK_SENSITIVITY);
+        blinkSensitivity = sharedPreferences.getInt("blink_sensitivity_preference", DEFAULT_BLINK_SENSITIVITY) / 100f;
+        detector.setSensitivity(blinkSensitivity);
     }
 
 }
